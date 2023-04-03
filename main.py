5563a249b7114967c05dec9c66086e3948c9fe0d
jinseo-ok <jinseock95@gmail.com>
2023-04-03 23:09:26 +0900

    add: unittest

--- main.py
+++ main.py
@@ -1,12 +1,9 @@
-def              helloworld(a):
+def helloworld(a):
     value = f"Hello MLOps World, {a}!"
     print(value)
-    
+
     return value
 
 
-if __name__        ==        "__main__":
-
-
-    
+if __name__ == "__main__":
     helloworld("Carvin")
